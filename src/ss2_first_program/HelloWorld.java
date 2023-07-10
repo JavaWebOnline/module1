23f6bd3a57c3595e2a6a02970b551d1de88e7c40
ngocquang95 <ngocquang12395@gmail.com>
2023-07-10 16:29:11 +0700

    First program

---
+++ src/ss2_first_program/HelloWorld.java
@@ -0,0 +1,12 @@
+package ss2_first_program;
+
+public class HelloWorld {
+    // phương thức main
+    // main enter
+    public static void main(String[] args) {
+        // viết mã (code) ở đây
+
+        // sout enter
+        System.out.println("Hello World");
+    }
+}
---
+++ src/ss3_java_overview/practice/Hello.java
@@ -0,0 +1,7 @@
+package ss3_java_overview.practice;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
