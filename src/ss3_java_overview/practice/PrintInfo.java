20a12569334e8fcbec79e21d2a3e73a1f53f97fa
ngocquang95 <ngocquang12395@gmail.com>
2023-07-11 14:35:52 +0700

    Print Info

---
+++ src/ss3_java_overview/practice/PrintInfo.java
@@ -0,0 +1,18 @@
+package ss3_java_overview.practice;
+
+public class PrintInfo {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+
+        float a = 2;
+        int b = 3;
+        // 2 + 3 = 5;
+
+        System.out.println(a + " + " + b + " = " + (a + b));
+        System.out.printf("%f + %d = %f", a, b, a + b);
+    }
+}
