1c0d87e73a4e771645acde2709a78a7e855cf6e5
ngocquang95 <ngocquang12395@gmail.com>
2023-07-13 14:29:16 +0700

    Compare Equals

---
+++ src/ss7_debug_string/CompareEquals.java
@@ -0,0 +1,15 @@
+package ss7_debug_string;
+
+public class CompareEquals {
+    public static void main(String[] args) {
+        String str1 = "Nguyễn Ngọc Quang";
+        String str2 = "Nguyễn Ngọc Quang";
+        String str3 = new String("Nguyễn Ngọc Quang");
+
+        System.out.println(str1 == str2); // true
+        System.out.println(str1.equals(str2)); // true
+
+        System.out.println(str1 == str3); // false
+        System.out.println(str1.equals(str3)); // true
+    }
+}
