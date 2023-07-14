af924fb6021d5009af67672e9c071e1f4cf26e35
ngocquang95 <ngocquang12395@gmail.com>
2023-07-14 13:46:34 +0700

    Declare And Initialize

---
+++ src/ss9_array/DeclareAndInitialize.java
@@ -0,0 +1,16 @@
+package ss9_array;
+
+import java.util.Arrays;
+
+public class DeclareAndInitialize {
+    public static void main(String[] args) {
+        int[] arr = new int[] {10, 20, 30};
+
+        int[] arr2 = {10, 20, 30};
+
+//        for(int i = 0; i < arr2.length; i++) {
+//            System.out.print(arr2[i] + "\t");
+//        }
+        System.out.println(Arrays.toString(arr2));
+    }
+}
